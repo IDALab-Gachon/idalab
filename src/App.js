009d759343a235494ebef3ef0ee0683a112d1722
xoyeop <bbusso90@gmail.com>
2020-03-30 13:23:55 +0900

    Modify navbar, home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo_typo_v1.svg';
+import logo from './logo_typo_v1.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home">
+          <img src={logo} height="80px" alt="logo"/>
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">HOME</Nav.Link>
+          <Nav.Link href="#member">MEMBERS</Nav.Link>
+          <Nav.Link href="#pub">PUBLICATIONS</Nav.Link>
+          <Nav.Link href="#project">PROJECTS</Nav.Link>
+        </Nav>
+      </Navbar>
+      <div className="App-body">
+        <div className="App-body-title">
+          <h3>Intelligent Data Analysis Lab.</h3>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          IDA Lab., Intelligent Data Analysis Laboratory, focuses on designing efficient algorithm or model to discover knowledge from big data. We research and solve the problems based on data mining, machine learning, and deep learning techniques across various fields with the aim of intelligent big data analysis in real-world data.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Our research directions are (1) scaling up algorithms to cope with Big Data, (2) improving the data analysis mechanism by auto-growing knowledge graph. In common, we first find a novel killer application for the emerging data types and then develop effective and efficient algorithms for the application. Overall, we are trying to go beyond traditional big data analysis approaches.
+        </p>
+        <p>
+        More specifically, our main research topics include but are not limited to
+        </p>
+        <ul>
+          <li>Intelligent data analysis technology for big data</li>
+          <li>Real-time data crawling & data analysis and prediction</li>
+          <li>Auto growing knowledge graph</li>
+          <li>Conversational AI framework technology</li>
+          <li>Intelligent emotional chatbot system</li>
+          <li>Automatic framework deep learning-based prediction model</li>
+          <li>Lifelong machine learning</li>
+        </ul>
+      </div>
     </div>
   );
 }
