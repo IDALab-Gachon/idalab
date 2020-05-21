b935e981a2b6df6fb470b7a0abb4c243df4af47d
xoyeop <bbusso90@gmail.com>
2020-05-22 04:18:13 +0900

    Add link to homepage

--- src/Pages/Members.js
+++ src/Pages/Members.js
@@ -124,6 +124,7 @@ const Members = () => {
               Associate Professor <br/>
               Dept. of AI & Sotware <br/>
               Gachon University <br/>
+              🌐 <a href="orjeong.gachon.ac.kr">orjeong.gachon.ac.kr</a> <br/>
               📧 orjeong@gachon.ac.kr
             </MemberInfo>
           </MemberCard>
